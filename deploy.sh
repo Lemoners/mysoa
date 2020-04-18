2331f27c851a5459415eb1fb6f3d4f6a2e392775
lemon <coderlemon17@163.com>
2020-04-18 11:37:34 +0800

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,4 @@
 tar -cpzf ser.tar.gz --exclude=./server/db/data ./server
 scp ./ser.tar.gz lemon@182.92.86.236:/home/lemon/Workspace/Nodejs
 rm ./ser.tar.gz
-ssh -p 22 lemon@182.92.86.236
-cd /home/lemon/Workspace/Nodejs
-rm -rf ./server
-tar -xpzf ser.tar.gz -C .
-rm ./ser.tar.gz
-tmux kill-server
-tmux
-node server/app.js
+ssh -p 22 lemon@182.92.86.236 "cd /home/lemon/Workspace/Nodejs; tar -xpzf ser.tar.gz -C .; rm ./ser.tar.gz; pkill node; nohup node server/app.js >/dev/null 2>&1 &"
